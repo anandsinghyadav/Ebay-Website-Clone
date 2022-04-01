3f505cfff2f1e84988fce5c8c31602c40d411db8
mahendrasingh-engineer <59507085+mahendrasingh-engineer@users.noreply.github.com>
2022-04-01 17:01:01 +0530

    connected pages
    I have just connected the pages electronics, smart phones & smart watches

---
+++ Mfooter2/footer.js
@@ -0,0 +1,49 @@
+const footer=()=>{
+
+    return `<div id="footer">
+    <div id="line"></div>
+
+    <div id="footerdiv">
+
+    <div id="footerOne">
+        <p><a href="https://www.ebayinc.com/company">About eBay</a></p>
+        <p><a href="https://community.ebay.com/t5/Announcements/bg-p/Announcements">Announcements</a></p>
+        <p><a href="https://community.ebay.com/">Community</a></p>
+        <p><a href="https://pages.ebay.com/securitycenter/index.html">Security Centers</a></p>
+        <p><a href="https://pages.ebay.com/seller-center/index.html">Seller Information Center</a></p>
+        <p><a href="https://www.ebay.com/help/policies/default/ebays-rules-policies?id=4205">Policies</a></p>
+        <p><a href="https://partnernetwork.ebay.com/">Affiliates</a></p>
+        <p><a href="https://www.ebay.com/help/home">Help & Contact </a></p>
+        <p><a href="https://pages.ebay.com/sitemap.html">Site Map </a></p>
+    </div>
+
+
+
+
+    <div id="footerTwo">
+        <p>Copyright @ 1995-2022 eBay Inc.All Rights Reserved.</p>
+        <p><a href="https://www.ebayinc.com/accessibility/">Accessibility</a></p>
+        <p><a href="https://pages.ebay.com/help/policies/user-agreement.html">User Agreement </a></p>
+        <p><a href="https://pages.ebay.com/help/policies/privacy-policy.html">Privacy</a></p>
+        <p><a href="https://pages.ebay.com/help/account/cookies-web-beacons.html">Cookies</a></p>
+        <p><a href="https://www.ebay.com/adchoice/ccpa">Do not sell my personal information </a></p>
+        <p>and</p>
+        <p><a href="https://www.ebay.com/adchoice">AdChoice</a></p>
+
+    </div>
+
+    <div id="nortan">
+    <img src="https://www.websecurity.digicert.com/content/dam/websitesecurity/icontextandtitle/Desktop/images/NortonSeal_PoweredbyDigiCert_80x40-02.png"
+    alt="" />
+    </div>
+</div>
+
+</div>`
+
+
+}
+
+export {footer}
+
+
+
