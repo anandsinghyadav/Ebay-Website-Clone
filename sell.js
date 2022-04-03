c3892c33bd0b26774b74c1a7c29ec23c9713e752
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-04-03 11:09:00 +0530

    Sachin

--- sell.js
+++ sell.js
@@ -1,18 +1,18 @@
 
 
     document.querySelector("#rightdata > ul:nth-child(2) > button").addEventListener("click", function () {
-        window.scrollBy(0, 908)
+        window.scrollBy(0, 700)
     })
 
     document.querySelector("#rightdata > ul:nth-child(1) > button").addEventListener("click", function () {
-        window.scrollBy(0, 557)
+        window.scrollBy(0,432)
 
     })
     document.querySelector("#rightdata > ul:nth-child(3) > button").addEventListener("click", function () {
-        window.scroll(0, 1200)
+        window.scroll(0, 1098)
     })
     document.querySelector("#rightdata > ul:nth-child(4) > button").addEventListener("click", function(){
-        window.scroll(0, 1630)
+        window.scroll(0, 1500)
     })
 
 // if( window.scrollBy(0, 557))
@@ -22,6 +22,10 @@ document.addEventListener("scroll",function()
     if(window.scrollY>500)
     {
         document.querySelector(".chamka").style.display="inline";
+    }
+    else{
+        document.querySelector(".chamka").style.display="none";
+
     }
     
 
